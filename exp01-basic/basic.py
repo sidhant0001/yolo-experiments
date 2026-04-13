a6c590dac173d03c82b52e659867d6e656dec189
sidhant0001 <sidhanthanda0001@gmail.com>
2026-04-12 23:33:32 -0400

    saving additional details

--- exp01-basic/basic.py
+++ exp01-basic/basic.py
@@ -1,3 +1,5 @@
+from os.path import exists
+
 from ultralytics import YOLO
 
 model = YOLO("yolo26n.pt")
@@ -16,4 +18,8 @@ model = YOLO("yolo26n.pt")
 result = model(
     "src-images/car.jpg",
     save=True,
+    exist_ok = True,
+    save_txt=True,
+    save_conf=True,
+    save_crop=True,
     project=r"C:\SID\Yolo\1-getting-started\exp01-basic\output-inference")
