1a636bc408ead427d1f3300e98c589ca163cf506
sidhant0001 <sidhanthanda0001@gmail.com>
2026-04-12 23:29:43 -0400

    version 3 of basic

--- exp01-basic/basic.py
+++ exp01-basic/basic.py
@@ -2,11 +2,18 @@ from ultralytics import YOLO
 
 model = YOLO("yolo26n.pt")
 
+#version1
 # results = model("https://ultralytics.com/images/bus.jpg", save=True)
 # print(results[0].boxes.cls)         # class IDs of what it found
 
+#version2
+# result = model(
+#     "src-images/car.jpg",
+#     save=True,
+#     project="output-inference")
 
+#version3
 result = model(
     "src-images/car.jpg",
     save=True,
-    project="output-inference")
+    project=r"C:\SID\Yolo\1-getting-started\exp01-basic\output-inference")
